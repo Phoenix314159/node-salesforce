8656081e751e4d77c508542d12ad80ddc43ca90d
James Langford <derivative7117@live.com>
2017-08-11 00:53:19 -0600

    added config

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    port: 8443,
+    clientID: '3MVG9zlTNB8o8BA2glVaX_J4n5gNrCT7fl6_ejglq4w8YN2Cz0E8ydbnzvyCZz1RmfkndEMwMO1nZs2EO47c4',
+    clientSecret: '4603062827925297068',
+    callbackUrl: 'https://salesforce-1256.herokuapp.com/oauth/callback',
+    session_secret: 'running_dog'
+}
