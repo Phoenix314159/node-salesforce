fb1fac08df47e72e62e20d9579a9601629b3dee4
James Langford <derivative7117@live.com>
2017-08-10 23:09:29 -0600

    dude

--- public/js/components/accounts.js
+++ public/js/components/accounts.js
@@ -1,6 +1,6 @@
 angular.module('salesforce').component('accounts', {
     templateUrl: './views/accounts.html',
-    controller: function () {
+    controller: function (mainService) {
         let vm = this;
 
         vm.showButton = true;
@@ -8,6 +8,12 @@ angular.module('salesforce').component('accounts', {
         vm.showData = () => {
             vm.showButton = false;
         }
+
+        vm.getAuth = mainService.getAuth().then(res => {
+            console.log(res);
+        })
+        vm.getAuth();
+
 
     }
 });
