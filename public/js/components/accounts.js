d12b863c14203496adc648c272dc888cca713629
James Langford <derivative7117@live.com>
2017-08-11 01:02:36 -0600

    added account data

--- public/js/components/accounts.js
+++ public/js/components/accounts.js
@@ -2,14 +2,9 @@ angular.module('salesforce').component('accounts', {
     templateUrl: './views/accounts.html',
     controller: function (mainService) {
         let vm = this;
-
         vm.showButton = true;
-
         vm.showData = () => {
             vm.showButton = false;
-        }
-
-        vm.showData = () => {
             mainService.getAccts().then(res => {
                 console.log(res.data);
                 vm.data = res.data;
