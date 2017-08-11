4d81f071dbbc1cc75ba6c6782dc2e51400f98bff
James Langford <derivative7117@live.com>
2017-08-10 18:19:13 -0600

    dude

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    port: 8443,
+    clientID: '3MVG9zlTNB8o8BA2glVaX_J4n5gNrCT7fl6_ejglq4w8YN2Cz0E8ydbnzvyCZz1RmfkndEMwMO1nZs2EO47c4',
+    clientSecret: '4603062827925297068',
+    callbackUrl: 'https://localhost:8443/oauth/callback',
+    session_secret: 'running_dog'
+}
+
